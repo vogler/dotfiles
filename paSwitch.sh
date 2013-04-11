eec1f338772168cf7879ab8a5c099f30cb38749d
Ralf Vogler <ralf.vogler@gmail.com>
2013-04-11 17:11:48 +0200

    use pamixer to change default sink added script to toggle between first two sinks

---
+++ paSwitch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# http://askubuntu.com/questions/4055/audio-output-device-fast-switch/18210#18210
+sinks=($(pacmd list-sinks | grep index | \
+    awk '{ if ($1 == "*") print "1",$3; else print "0",$2 }'))
+inputs=($(pacmd list-sink-inputs | grep index | awk '{print $2}'))
+
+[[ ${sinks[0]} = 0 ]] && swap=${sinks[1]} || swap=${sinks[3]}
+
+pacmd set-default-sink $swap &> /dev/null
+for i in ${inputs[*]}; do pacmd move-sink-input $i $swap &> /dev/null; done
+
