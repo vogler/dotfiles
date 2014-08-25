98a2fb8022e8c80345c6a3ff2482e40ccb3ac714
Ralf Vogler <ralf.vogler@gmail.com>
2014-08-25 16:29:19 +0200

    gitdrop.sh apply: abort if repo is dirty

--- gitdrop.sh
+++ gitdrop.sh
@@ -1,11 +1,20 @@
+set -e
 path=~/Dropbox/github
 mkdir -p $path
 remote=${2-"origin"}
 repo=$(git remote show $remote | grep -P -o "github.com/.+$" | uniq | sed 's/github.com\///' | sed 's/\.git$//' | sed 's/\//:/g')
 branch=$(git branch | grep "*" | cut -c3-)
 file=$path/$repo:$branch.diff
+function git_dirty {
+    dir=${1-"`pwd`"};
+    test -n "$(git -C $dir  status --porcelain)"
+}
 case $1 in
     apply)  if [ -f $file ]; then
+                if git_dirty; then
+                    echo "This repo is dirty. Take care of that first!"
+                    exit
+                fi
                 git apply $file
             else
                 echo "$file does not exist!"
