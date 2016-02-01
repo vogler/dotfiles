3b7561106e9521e6418a235b61f142a541cfb64b
Ralf Vogler <ralf.vogler@gmail.com>
2016-02-01 17:17:05 +0100

    args for deleteTrailingSpaces.sh

--- deleteTrailingSpaces.sh
+++ deleteTrailingSpaces.sh
@@ -1,1 +1,4 @@
-find src/ -name "*.ml" -type f -print0 | xargs -0 sed -i 's/[ \t]*$//'
+name=${1-"*.ml"}
+path=${2-"."}
+find "$path" -name "$name" -type f -print0 | xargs -0 sed -i 's/[ \t]*$//'
+# find "$path" -name "$name" -type f -print0 | xargs -0 echo
