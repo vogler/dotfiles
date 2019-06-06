905c882c77195e54c66554b586ec82fa49554f86
Ralf Vogler <ralf.vogler@gmail.com>
2019-06-06 17:48:27 +0200

    install brew packages, TODO keep this updated, add install time and options like --HEAD

--- init.sh
+++ init.sh
@@ -1,16 +1,36 @@
 set -e
+
+echo ">> Get submodules"
+git submodule update --init --recursive
+
 # PACKAGES
-##########
+if [ "$(uname)" == "Darwin" ]; then
+  echo ">> [Running macOS]"
+
+  if test ! $(which brew); then
+      echo ">> Install homebrew"
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+
+  echo ">> brew tap ..."
+  source install/brew-tap.sh
+  echo ">> brew install ..."
+  source install/brew.sh
+  echo ">> brew cask install ..."
+  source install/brew-cask.sh
+else
+  echo ">> [Running Linux]"
+
+  echo ">> apt install ..."
+  source install/apt.sh
+fi
 # https://wiki.archlinux.org/index.php/Pacman_Tips#Backing_up_and_retrieving_a_list_of_installed_packages
 # backup installed packages (alternatively use aura -B and auro -Br)
 #comm -23 <(pacman -Qeq|sort) <(pacman -Qmq|sort) > pkglist.txt
 # restore
 #pacman -S --needed $(comm -12 <(pacman -Slq|sort) <(sort badpkdlist) )
 
-# get submodules
-git submodule update --init --recursive
-
-# ZSH
+echo ">> Link prezto for zsh"
 ln -sf `pwd`/.zprezto ~
 cat <<EOT | zsh
 setopt EXTENDED_GLOB
@@ -19,18 +39,22 @@ for rcfile in "\${ZDOTDIR:-\$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 EOT
 if [ $SHELL != "/bin/zsh" ]; then
-        chsh -s /bin/zsh
+    echo ">> Set zsh as default shell"
+    chsh -s /bin/zsh
 fi
 
-# VIM
-#####
+echo ">> Link vim"
 ln -sf `pwd`/.vimrc ~
+echo ">> Install vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo ">> Install vim plugins"
 vim +PlugInstall +qall
 
 # https://github.com/tpope/vim-sensible
 # otherwise it puts the files in the current directory
 mkdir -p ~/.vim/{swap,backup,undo}
 
-# tmux plugin manager
+echo ">> Install Tmux Plugin Manager"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo ">> Done"
---
+++ install/apt.sh
@@ -0,0 +1,2 @@
+# apt list --installed
+# TODO
