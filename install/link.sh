08b1e7441e12df8aa6bed48b4f08edcefdfaa8a4
Ralf Vogler <ralf.vogler@gmail.com>
2019-07-19 16:52:43 +0200

    dotfiles must be placed directly into ~ for symlinks to be put in the right place

--- install/link.sh
+++ install/link.sh
@@ -5,7 +5,7 @@ echo ">> Find *.symlink in $BASEDIR"
 
 tolink=$( find -H "$BASEDIR" -name '*.symlink' )
 for file in $tolink ; do
-    target=$(echo $file | sed 's/\.symlink$//' | sed 's/\/dotfiles//')
+    target=$(echo $file | sed 's/\.symlink$//' | sed 's/\/dotfiles//') # remove /dotfiles from the absolute path -> root is parent of /dotfiles -> problem if /dotfiles is not placed directly into ~
     echo "ln -s $file $target"
     mkdir -p $(dirname $target)
     ln -sf $file $target
