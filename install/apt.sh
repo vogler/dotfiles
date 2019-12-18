187675cf7edb4cd0ecf0855f1c251c166ca37c11
Ralf Vogler <ralf.vogler@gmail.com>
2019-12-18 22:40:43 +0100

    install: some essentials in apt.sh

--- install/apt.sh
+++ install/apt.sh
@@ -1,2 +1,8 @@
-# apt list --installed
-# TODO
+agi() {
+  sudo apt-get install -yy --fix-missing "$@"
+}
+sudo apt update
+# apt list --installed | sed -e 's/\(.*\)\/.*/agi \1/' # TODO versions? do I really want all packages?
+agi zsh
+agi tmux
+agi neovim
