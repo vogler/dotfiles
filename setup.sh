cecd4f3011080a2df6b293e3b1da562b274bf984
Ralf Vogler <ralf.vogler@gmail.com>
2020-01-03 14:53:05 +0100

    install: `git config --add` really adds a new line instead of changing the value

--- setup.sh
+++ setup.sh
@@ -77,8 +77,8 @@ ln -sf `pwd`/.dir_colors ~
 # git
 ln -sf `pwd`/.gitconfig ~
 # no branching in .gitconfig -> set OS-specific config via commands:
-[ "$(uname)" == "Darwin" ] && sudo git config --system --add credential.helper osxkeychain
-[ "$(uname)" == "Linux" ] && sudo git config --system --add credential.helper 'cache --timeout=604800'
+[ "$(uname)" == "Darwin" ] && sudo git config --system --replace-all credential.helper osxkeychain
+[ "$(uname)" == "Linux" ] && sudo git config --system --replace-all credential.helper 'cache --timeout=604800'
 if [ "$(uname)" == "Linux" ] && ! has diff-highlight; then # only needed because diff-so-fancy fails when used as interactive.diffFilter
   # this is only a problem on Debian https://bugs.launchpad.net/ubuntu/+source/git/+bug/1713690
   sudo make -B -C /usr/share/doc/git/contrib/diff-highlight diff-highlight
