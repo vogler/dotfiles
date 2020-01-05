275b411223bcc06ff15a735e74deb57d5e413308
Ralf Vogler <ralf.vogler@gmail.com>
2020-01-05 18:50:42 +0100

    windows: display-off

--- windows/ctrl-server/ctrl.js
+++ windows/ctrl-server/ctrl.js
@@ -13,16 +13,15 @@ const ahk = (file) => (os == 'win32' ? '' : '/mnt/c/Program\\ Files/AutoHotkey/A
 // http://www.nirsoft.net/utils/nircmd.html would need to be installed for `nircmd.exe standby`, `nircmd.exe monitor off`
 const cmds = {
   'suspend': ahk('suspend'), // DllCall via AutoHotkey. Find direct way to do this? `rundll32.exe powrprof.dll,SetSuspendState 0,0,0` also works but shouldn't be used according to https://stackoverflow.com/questions/37031935/setting-windows-to-energy-save-mode-using-node-js
+  'display-off': ahk('display-off'),
 }
 
 const requestHandler = (request, response) => {
   console.log(new Date().toLocaleString(), request.url)
   response.end(`handled ${request.url}`) // should send some response before going into standby, otherwise the request in node-red errors with a timeout
-  switch (request.url) {
-    case '/suspend':
-      execSync(cmds.suspend)
-      break;
-  }
+  const cmd = request.url.substring(1)
+  if (cmd in cmds) execSync(cmds[cmd])
+  else console.log(`unknown command ${cmd}`)
 }
 
 const server = http.createServer(requestHandler)
---
+++ windows/ctrl-server/display-off.ahk
@@ -0,0 +1,1 @@
+SendMessage, 0x112, 0xF170, 2,, Program Manager
