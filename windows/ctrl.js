fdfdfd79740687b77126000343008ace68885563
Ralf Vogler <ralf.vogler@gmail.com>
2020-01-05 16:35:58 +0100

    windows: ctrl.js: ahk wrapper for WSL

--- windows/ctrl.js
+++ windows/ctrl.js
@@ -1,15 +1,27 @@
-// run with `node ctrl.js` under Windows (or WSL with alternative cmd below)
+// run with `node ctrl.js` on Windows or WSL
+
 const http = require('http')
+const { execSync } = require('child_process')
+
+const os = require('os').platform()
+console.log('running on', os)
+
+// config
 const port = 3000
-const { execSync } = require('child_process');
-const cmd = 'suspend.ahk' // DllCall via AutoHotkey. Find direct way to do this?
-// const cmd = '/mnt/c/Program\ Files/AutoHotkey/AutoHotkeyU64.exe suspend.ahk' // WSL does not know how to run .ahk files, so we need to add the path to AutoHotkey
+// WSL does not know how to run .ahk files, so we need to prepend the path to AutoHotkey
+const ahk = (file) => (os == 'win32' ? '' : '/mnt/c/Program\\ Files/AutoHotkey/AutoHotkeyU64.exe ') + file + '.ahk'
+const cmds = {
+  'suspend': ahk('suspend'), // DllCall via AutoHotkey. Find direct way to do this?
+}
 
 const requestHandler = (request, response) => {
   console.log(new Date().toLocaleString(), request.url)
   response.end(`handled ${request.url}`) // should send some response before going into standby, otherwise the request in node-red errors with a timeout
-  if(request.url == '/standby')
-    execSync(cmd)
+  switch (request.url) {
+    case '/standby':
+      execSync(cmds.suspend)
+      break;
+  }
 }
 
 const server = http.createServer(requestHandler)
