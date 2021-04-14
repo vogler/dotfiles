f7937952a8d015131c26a23577575755ed1e17bd
Ralf Vogler <ralf.vogler@gmail.com>
2021-04-14 16:08:07 +0200

    bitbar often has DNS lookup error for rpi3 -> use IP

--- bin/co2.sh
+++ bin/co2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export PATH='/usr/local/bin:$PATH' # this is only needed for BitBar on macOS
-mosquitto_sub -h rpi3 -t 'sensors/mh-z19b' -C 1 | jq .co2
+rpi3=rpi3
+rpi3=192.168.178.100 # sometimes DNS lookup fails. why?
+mosquitto_sub -h $rpi3 -t 'sensors/mh-z19b' -C 1 | jq .co2
