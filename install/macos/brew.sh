647e2d2f2023ec15a7a728b5931bc9d9a6046922
Ralf Vogler <ralf.vogler@gmail.com>
2021-07-26 18:30:02 +0200

    `brew install` more tools
    https://github.com/lunaryorn/dotfiles/commit/2d8531881c2bc61f091d83bc2cf02ad6ebb680c6

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -38,29 +38,44 @@ brew install iina # media player
 
 # sys tools
 brew install coreutils # newer than macOS
-brew install grep # newer than macOS: 3.6 vs. 2.5.1-FreeBSD
+brew install grep # newer than macOS: 3.6 vs 2.5.1-FreeBSD
+brew install less # newer than macOS: 590 vs 487
+brew install rsync # newer than macOS: 3.2.3 vs 2.6.9
 brew install htop
-brew install tree
+brew install tree # `exa --tree --level=2` has colors and can show meta-data with --long
+brew install tldr # `tldr tar` shorter than `curl cheat.sh/tar` and works offline
+brew install bat # cat with syntax highlighting and Git integration
+brew install fd # Simple, fast and user-friendly alternative to find
+brew install the_silver_searcher # Code-search similar to ack, but faster
+brew install exa # Modern replacement for 'ls'
+brew install p7zip # 7z with support for Brotli, Fast LZMA2, LZ4, LZ5, Lizard and Zstd
 
 # network
-brew install wget
-brew install mosquitto
+brew install wget # alt for curl
+brew install httpie # https://httpie.io User-friendly cURL replacement
+brew install mosquitto # MQTT pub/sub
+brew install youtube-dl # download videos from many websites
 
 # dev tools
 brew install neovim
 brew install iterm2
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
 brew install tmux
-brew install the_silver_searcher
-brew install fd
-brew install rlwrap
+brew install tokei # count code lines fast and by language
+brew install rlwrap # used in ocaml alias since it has no readline support; use utop for better REPL
 brew install pkg-config
+
+# data processing
+brew install jq # JSON CLI processor
+brew install xsv # CSV CLI: cat count flatten frequency join search select slice sort split stats table
+brew install jid # Json incremental digger
 
 # git
 brew install diff-so-fancy
 brew install hub
 brew install gh # https://github.com/cli/cli/blob/trunk/docs/gh-vs-hub.md
 brew install gitwatch # use GitDoc in vscode instead? https://marketplace.visualstudio.com/items?itemName=vsls-contrib.gitdoc
+brew install git-recent # could use `git branch --sort=-committerdate`, but this has nice default format
 
 # programming
 brew install node # JavaScript
@@ -92,3 +107,11 @@ brew install font-consolas-for-powerline font-menlo-for-powerline font-meslo-for
 # tracking
 # brew install rescuetime # Intel, fails because it requires Rosetta 2 (`sudo softwareupdate --install-rosetta`). Why did the other Intel apps work without installing this?
 # WhatPulse neither on brew or app store. Download from https://whatpulse.org
+
+# image/video
+# brew install imagemagick
+# brew install ffmpeg
+brew install --cask handbrake # video transcoder; also available as formula, but: A full installation of Xcode.app 10.3 is required to compile
+
+# for more see install/macos/old-MBP/Brewfile
+# Brewfile in https://github.com/lunaryorn/dotfiles/commit/2d8531881c2bc61f091d83bc2cf02ad6ebb680c6
