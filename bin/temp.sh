74a70b135f9c79bdb09da1eb5969d2ec9bf78c02
Ralf Vogler <ralf.vogler@gmail.com>
2021-08-05 00:51:31 +0200

    macos/bitbar: #!/bin/zsh since .bash_profile is not read

--- bin/temp.sh
+++ bin/temp.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-export PATH='/usr/local/bin:$PATH' # this is only needed for BitBar on macOS
+#!/bin/zsh
+# export PATH='/usr/local/bin:$PATH' # this is needed for BitBar on macOS when using bash above (ignores ~/.bash_profile) instead of zsh.
 rpi3=rpi3
 rpi3=192.168.178.100 # sometimes DNS lookup fails. why?
 mosquitto_sub -h $rpi3 -t 'sensors/bme280' -C 1 | jq .temperature
--- macos/bitbar/fan.15s.sh
+++ macos/bitbar/fan.15s.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-# export PATH='/usr/local/bin:$PATH' # this is only needed for BitBar on macOS
+#!/bin/zsh
+# export PATH='/usr/local/bin:$PATH' # this is needed for BitBar on macOS when using bash above (ignores ~/.bash_profile) instead of zsh.
+# using absolute path for v1 so the above does not matter:
 /usr/local/opt/influxdb@1/bin/influx -execute 'SELECT "fan" FROM "telegraf"."autogen"."thermal" order by time desc limit 1' -format=csv | tail -n1 | cut -d, -f3
