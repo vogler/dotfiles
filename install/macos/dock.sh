70d92f617859fc555e04baa4ca30f4b8fd24b3d2
Ralf Vogler <ralf.vogler@gmail.com>
2021-10-11 17:03:00 +0200

    dock: WhatsChrome instead of WhatsApp (still Intel)

--- install/macos/dock.sh
+++ install/macos/dock.sh
@@ -11,5 +11,6 @@ dockutil --add /Applications/Google\ Chrome.app --replacing 'Google Chrome' --po
 dockutil --add /Applications/iTerm.app --replacing iTerm --position 4
 # dockutil --add /Applications/Spotify.app --replacing Spotify --position 5
 dockutil --add ~/Applications/Chrome\ Apps.localized/Spotify.app --replacing Spotify --position 5
-dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 6
+# dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 6
+dockutil --add ~/Applications/Chrome\ Apps.localized/WhatsChrome.app --replacing WhatsChrome --position 6
 dockutil --add /Applications/Slack.app --replacing Slack --position 7
