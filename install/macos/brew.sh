b2c7ca707e9e73e52b36e732135242ac2d5b6608
Ralf Vogler <ralf.vogler@gmail.com>
2022-03-31 16:50:51 +0200

    brew install --cask docker

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -99,6 +99,7 @@ brew install autoconf automake # autotools
 brew install heroku/brew/heroku
 # mas install 1388020431 # DevCleaner for Xcode: cleans cache files (none if not actually using Xcode)
 brew install devutils # 48MB, demo, GUI with converters for unix time, base64, URLs, regex, JSON, CSV, Markdown etc.
+brew install --cask docker # 1.5 GB, hate it, only gave in for avoiding captcha with headless playwright
 
 # git
 brew install diff-so-fancy # within-line highlights
