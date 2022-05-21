3d2f23dffde923495a98926c085db550087f0c8e
Ralf Vogler <ralf.vogler@gmail.com>
2022-05-21 11:28:05 +0200

    use yt-dlp instead of youtube-dl

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -123,7 +123,8 @@ brew install postgresql # SQL DB; `brew services start postgresql` to enable sta
 brew install wget # alt for curl
 brew install httpie # https://httpie.io User-friendly cURL replacement
 brew install mosquitto # MQTT pub/sub
-brew install youtube-dl # download videos from many websites
+# brew install youtube-dl # download videos from many websites -> yt only ~70KB/s -> replaced with yt-dlp which dl from yt with full speed
+brew install yt-dlp/taps/yt-dlp # youtube-dl fork with additional features and fixes: full dl speed, SponsorBlock, yt-dlp --cookies-from-browser chrome --max-downloads 10 --sponsorblock-remove default :ytwatchlater
 brew install angry-ip-scanner
 # brew install insomnia # Intel, 337MB; HTTP and GraphQL Client
 mas install 1451685025 # WireGuard VPN client; brew's wireguard-tools has no GUI
