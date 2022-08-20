68c145da9a9bb984964b23a2ba3e63c1e2e96c49
Ralf Vogler <ralf.vogler@gmail.com>
2022-08-21 00:33:49 +0200

    apt update silently

--- install/apt.sh
+++ install/apt.sh
@@ -52,7 +52,7 @@ agi mmv # move/copy/append/link multiple files by wildcard patterns
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
+sudo apt update -qq
 agi gh 
 
 agi youtube-dl
@@ -63,7 +63,7 @@ sudo chmod a+rx /usr/local/bin/yt-dlp
 # https://gitlab.com/volian/nala - nicer frontend around apt with pretty formatting, parallel downloads, `nala fetch` to select the fastest mirrors, and `nala history` to undo/redo
 echo "deb https://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
-sudo apt update
+sudo apt update -qq
 agi nala || agi nala-legacy
 
 if [[ "$*" == *latex* ]]; then
