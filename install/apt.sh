d904d4ad2ddfac245971ab8613381c153a13204d
Ralf Vogler <ralf.vogler@gmail.com>
2022-08-21 00:18:16 +0200

    update before gh install

--- install/apt.sh
+++ install/apt.sh
@@ -48,7 +48,9 @@ agi mmv # move/copy/append/link multiple files by wildcard patterns
 
 # GitHub CLI: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
 agi gh 
 
 agi youtube-dl
