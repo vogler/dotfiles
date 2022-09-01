d6dc2d0fff34ac4f381dd1285aca395a2153f9ad
Ralf Vogler <ralf.vogler@gmail.com>
2022-09-01 20:49:30 +0200

    snap install vscode on chromeos

--- install/apt.sh
+++ install/apt.sh
@@ -24,6 +24,7 @@ if [[ -d /mnt/chromeos ]]; then
   echo "Right click on Terminal > Shut down Linux -- really needed?"
   sudo systemctl enable --now snapd
   # systemctl status snapd
+  sudo snap install code --classic # Visual Studio Code
 fi
 sudo snap install nvim --classic # Virtuozzo/OpenVZ: https://community.letsencrypt.org/t/system-does-not-fully-support-snapd-cannot-mount-squashfs-image-using-squashfs/132689/2
 agi tig # Text interface for Git repositories
