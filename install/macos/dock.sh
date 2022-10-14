453f3cabf4495361d67fcfb0d3fd07decf6a2cc0
Ralf Vogler <ralf.vogler@gmail.com>
2022-10-14 18:58:31 +0200

    macos dock rearrange

--- install/macos/dock.sh
+++ install/macos/dock.sh
@@ -6,12 +6,15 @@ dockutil --remove 'Contacts'
 dockutil --remove 'Keynote'
 dockutil --remove 'Numbers'
 dockutil --remove 'Pages'
+dockutil --remove 'Music'
+dockutil --remove 'Podcasts'
 
 dockutil --add /Applications/Google\ Chrome.app --replacing 'Google Chrome' --position 3
-dockutil --add /Applications/iTerm.app --replacing iTerm --position 4
+# dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 4
+dockutil --add ~/Applications/Chrome\ Apps.localized/WhatsApp.app --replacing WhatsApp --position 4
 # dockutil --add /Applications/Spotify.app --replacing Spotify --position 5
 dockutil --add ~/Applications/Chrome\ Apps.localized/Spotify.app --replacing Spotify --position 5
-# dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 6
-dockutil --add ~/Applications/Chrome\ Apps.localized/WhatsApp.app --replacing WhatsApp --position 6
-dockutil --add /Applications/Slack.app --replacing Slack --position 7
-dockutil --add ~/Applications/Autodesk\ Fusion\ 360.app --replacing 'Autodesk Fusion 360' --position 8
+dockutil --add /Applications/iTerm.app --replacing iTerm --position 6
+dockutil --add /Applications/Visual\ Studio\ Code.app --replacing 'Visual Studio Code' --position 7
+dockutil --add /Applications/Slack.app --replacing Slack --position 8
+dockutil --add ~/Applications/Autodesk\ Fusion\ 360.app --replacing 'Autodesk Fusion 360' --position 9
