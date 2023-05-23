806ca6593ce10d388f551577d088df7c26e1ea38
Ralf Vogler <ralf.vogler@gmail.com>
2023-05-23 19:48:45 +0200

    raycast: script for co2.sh - compact mode

---
+++ macos/raycast/scripts/co2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title CO2
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🫁
+
+# Documentation:
+# @raycast.description Show CO2 level.
+# @raycast.author Ralf Vogler
+# @raycast.authorURL https://github.com/vogler
+
+source ~/dotfiles/bin/co2.sh
