489f729ef1a15b60e63ccf63efe8d103a4ddfc35
Ralf Vogler <ralf.vogler@gmail.com>
2024-03-28 11:50:13 +0100

    bench-chrome.js starts a server, opens it in new tab and then closes everything on js fetch

---
+++ bin/stats/bench-chrome.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// This script measures the time it takes to open a new [tab]/window in Chrome, load some [html] and execute [js] which then closes the server and the window.
+// Requires the `chrome-cli` package to be installed globally: `npm install -g chrome-cli` or `brew install chrome-cli`.
+
+var http = require('http');
+const port = process.env.PORT || 8080;
+
+const date = () => (new Date()).toLocaleString('de');
+console.log(date(), `Starting server on http://localhost:${port}`);
+
+console.time('total');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if (req.url === '/') {
+    res.write(`<script>fetch("http://localhost:${port}/js").then(_ => window.close());</script>`);
+    console.timeEnd('html');
+    console.time('js');
+  } else if (req.url === '/js') {
+    res.write('ok');
+    console.timeEnd('js');
+    console.timeEnd('browser');
+    server.close();
+  }
+  res.end();
+});
+server.on('close', () => {
+  console.timeEnd('total');
+});
+server.listen(port);
+
+const { execSync } = require('child_process');
+console.time('browser');
+console.time('tab');
+execSync(`chrome-cli open "http://localhost:${port}" -n`);
+console.timeEnd('tab');
+
+console.time('html');
