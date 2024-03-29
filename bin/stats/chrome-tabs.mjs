73ad5fb70ad54df8184a388b131f86f92dffbb0c
Ralf Vogler <ralf.vogler@gmail.com>
2024-03-29 09:19:29 +0100

    chrome-tabs.mjs to save active windows/tabs using chrome-cli

---
+++ bin/stats/chrome-tabs.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env -S zx --install
+// Docs: https://google.github.io/zx/api
+
+$.verbose = false
+
+import { log, error } from 'console'
+const errorr = (...a) => error(...a.map(x => chalk.red(x))) // can't redeclare error...
+
+const j = JSON.parse(await $`OUTPUT_FORMAT=json chrome-cli list tabs`)
+// log(j)
+// output is flawed since active tab in window is only indentified via title which need not be unique
+
+const windows = {}
+for (const tab of j.tabs) {
+  if (!(tab.windowId in windows)) {
+    windows[tab.windowId] = { activeTab: null, tabs: {} } // `title: tab.windowName` redundant via activeTab
+  }
+  const window = windows[tab.windowId]
+  if (tab.windowName == tab.title) window.activeTab = tab.id
+  window.tabs[tab.id] = { title: tab.title, url: tab.url }
+}
+
+console.dir(windows, { depth: null })
