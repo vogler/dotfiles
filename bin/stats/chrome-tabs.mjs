37e9bc6be4640cb1e301dae1677e806a3645f13e
Ralf Vogler <ralf.vogler@gmail.com>
2024-12-23 14:27:33 +0100

    chrome-tabs.mjs example input, refactor

--- bin/stats/chrome-tabs.mjs
+++ bin/stats/chrome-tabs.mjs
@@ -7,15 +7,21 @@ import { log, error } from 'console'
 const errorr = (...a) => error(...a.map(x => chalk.red(x))) // can't redeclare error...
 
 const j = JSON.parse(await $`OUTPUT_FORMAT=json chrome-cli list tabs`)
-// log(j)
-// output is flawed since active tab in window is only indentified via title which need not be unique
+// log(j) // example:
+// {
+//   "tabs": [
+//     {
+//       "id": "1057768852",
+//       "title": "Inbox (9) - Gmail",
+//       "url": "https://mail.google.com/mail/u/0/#inbox",
+//       "windowId": "1057768851",
+//       "windowName": "Inbox (9) - Gmail"
+//     }, // ...
+// output is flawed since active tab in window is only indentified via (title == windowName) which need not be unique
 
 const windows = {}
 for (const tab of j.tabs) {
-  if (!(tab.windowId in windows)) {
-    windows[tab.windowId] = { activeTab: null, tabs: {} } // `title: tab.windowName` redundant via activeTab
-  }
-  const window = windows[tab.windowId]
+  const window = windows[tab.windowId] ??= { activeTab: null, tabs: {} } // `title: tab.windowName` redundant via activeTab
   if (tab.windowName == tab.title) window.activeTab = tab.id
   window.tabs[tab.id] = { title: tab.title, url: tab.url }
 }
