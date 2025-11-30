b8e7d2fe21821bdb0a5ce22b892e9b36dfe7ccca
Ralf Vogler <ralf.vogler@gmail.com>
2025-11-30 16:48:03 +0100

    co2 via ssh for remote access

--- bin/co2.sh
+++ bin/co2.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 # export PATH='/usr/local/bin:$PATH' # this is needed for BitBar on macOS when using bash above (ignores ~/.bash_profile) instead of zsh.
-rpi3=rpi3
-# rpi3=192.168.178.3 # sometimes DNS lookup fails. why?
-mosquitto_sub -h $rpi3 -t 'sensors/mh-z19b' -C 1 | jq .co2
+# run via ssh for remote access (Host rpi3 defined in ~/.ssh/config)
+ssh rpi3 mosquitto_sub -t 'sensors/mh-z19b' -C 1 | jq .co2
