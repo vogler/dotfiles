84d4b8bc27d4f03d67e08fa27d2d7d3786059cd1
Ralf Vogler <ralf.vogler@gmail.com>
2026-06-18 13:59:20 +0200

    brew install claude claude-code

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -610,10 +610,13 @@ brew install ollama # 25M, Create, run, and share large language models (LLMs) (
 brew install lm-studio # 1.5G, Discover, download, and run local LLMs (2025-11-08T11:54:27+01:00)
 # brew tap jundot/omlx https://github.com/jundot/omlx
 # brew install omlx # LLM inference server with continuous batching & SSD caching for Apple Silicon — managed from the macOS menu bar
-# LLM clients
-brew install opencode # 101M, AI coding agent, built for the terminal (2025-10-21T19:07:16+02:00)
-brew install gemini-cli # 116M, Interact with Google Gemini AI models from the command-line (2026-05-11T19:00:13+02:00) - already annoying (randomly cleared terminal...)
+# LLM desktop clients
 brew install google-gemini # 174M, Native desktop AI assistant from Google (2026-05-11T19:05:04+02:00)
+brew install claude # 707M, Anthropic's official Claude AI desktop app (2026-06-18T13:39:06+02:00)
+# LLM CLI clients
+brew install gemini-cli # 116M, Interact with Google Gemini AI models from the command-line (2026-05-11T19:00:13+02:00) - already annoying (randomly cleared terminal...)
+brew install claude-code # 53G, Terminal-based AI coding assistant (2026-06-18T13:44:55+02:00)
+brew install opencode # 101M, AI coding agent, built for the terminal (2025-10-21T19:07:16+02:00)
 brew install pi-coding-agent # 157M, AI agent toolkit (2026-05-11T19:09:46+02:00) - simpler than claude/opencode etc., but has things like /tree for branching and can be customized via TS
 
 # not available in brew or mas:
