f36192e0ffc795c104049451f8d4b7193a14ea2a
Ralf Vogler <ralf.vogler@gmail.com>
2026-08-24 12:32:08 +0200

    update dock.sh: non-Chrome WhatsApp & Spotify, iTerm -> Ghostty

--- install/macos/dock.sh
+++ install/macos/dock.sh
@@ -1,3 +1,4 @@
+dockutil --remove 'Launchpad'
 dockutil --remove 'Messages'
 dockutil --remove 'Mail'
 dockutil --remove 'FaceTime'
@@ -10,11 +11,12 @@ dockutil --remove 'Music'
 dockutil --remove 'Podcasts'
 
 dockutil --add /Applications/Google\ Chrome.app --replacing 'Google Chrome' --position 3
-# dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 4
-dockutil --add ~/Applications/Chrome\ Apps.localized/WhatsApp.app --replacing WhatsApp --position 4
-# dockutil --add /Applications/Spotify.app --replacing Spotify --position 5
-dockutil --add ~/Applications/Chrome\ Apps.localized/Spotify.app --replacing Spotify --position 5
-dockutil --add /Applications/iTerm.app --replacing iTerm --position 6
-dockutil --add /Applications/Visual\ Studio\ Code.app --replacing 'Visual Studio Code' --position 7
-dockutil --add /Applications/Slack.app --replacing Slack --position 8
-dockutil --add ~/Applications/Autodesk\ Fusion\ 360.app --replacing 'Autodesk Fusion 360' --position 9
+# dockutil --add ~/Applications/Chrome\ Apps.localized/WhatsApp.app --replacing WhatsApp --position 4
+dockutil --add /Applications/WhatsApp.app --replacing WhatsApp --position 4
+# dockutil --add ~/Applications/Chrome\ Apps.localized/Spotify.app --replacing Spotify --position 5
+dockutil --add /Applications/Spotify.app --replacing Spotify --position 5
+dockutil --add /Applications/Slack.app --replacing Slack --position 6
+# dockutil --add /Applications/iTerm.app --replacing iTerm --position 7
+dockutil --add /Applications/Ghostty.app --replacing Ghostty --position 7
+dockutil --add /Applications/Visual\ Studio\ Code.app --replacing 'Visual Studio Code' --position 8
+# dockutil --add ~/Applications/Autodesk\ Fusion\ 360.app --replacing 'Autodesk Fusion 360' --position 9
