ac24bb1fda8addc513242f28ddb254de883231ba
Ralf Vogler <ralf.vogler@gmail.com>
2026-08-28 11:39:51 +0200

    make co2.sh also show temp and humidity from bme280

--- bin/co2.sh
+++ bin/co2.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 # export PATH='/usr/local/bin:$PATH' # this is needed for BitBar on macOS when using bash above (ignores ~/.bash_profile) instead of zsh.
 # run via ssh for remote access (Host rpi3 defined in ~/.ssh/config)
-ssh rpi3 mosquitto_sub -t 'sensors/mh-z19b' -C 1 | jq .co2
+ssh rpi3 "mosquitto_sub -t 'sensors/mh-z19b' -C 1; mosquitto_sub -t 'sensors/bme280' -C 1" |
+  jq -s -r '"\(.[0].co2) ppm, \(.[1].temperature)°C, \(.[1].humidity | round)%"'
--- macos/raycast/scripts/co2.sh
+++ macos/raycast/scripts/co2.sh
@@ -2,14 +2,14 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title CO2
+# @raycast.title Climate (CO2, Temp, Humidity)
 # @raycast.mode compact
 
 # Optional parameters:
 # @raycast.icon 🫁
 
 # Documentation:
-# @raycast.description Show CO2 level.
+# @raycast.description Show CO2, temperature, and humidity level.
 # @raycast.author Ralf Vogler
 # @raycast.authorURL https://github.com/vogler
 
